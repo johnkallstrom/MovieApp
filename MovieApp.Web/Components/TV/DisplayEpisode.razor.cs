9d9ad0bd4f34d0f98476094aab60348887c59aff
John Källström <jekallstrom@gmail.com>
2021-01-28 15:48:36 +0100

    Season details page fixes

---
+++ MovieApp.Web/Components/TV/DisplayEpisode.razor.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+using MovieApp.Web.Models;
+
+namespace MovieApp.Web.Components.TV
+{
+    public partial class DisplayEpisode
+    {
+        [Parameter]
+        public Episode Episode { get; set; }
+    }
+}
