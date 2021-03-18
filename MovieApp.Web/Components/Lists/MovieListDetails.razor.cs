e718b1f5892c18c6e872abb23f45d463fbc1138a
John Källström <jekallstrom@gmail.com>
2021-03-18 10:53:51 +0100

    Add layout / markup for items in list details page

--- MovieApp.Web/Components/Lists/MovieListDetails.razor.cs
+++ MovieApp.Web/Components/Lists/MovieListDetails.razor.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Configuration;
+using MovieApp.Domain.Models;
+using MovieApp.Web.Helpers;
 using MovieApp.Web.Services;
 using System.Threading.Tasks;
 
@@ -6,6 +9,12 @@ namespace MovieApp.Web.Components.Lists
 {
     public partial class MovieListDetails
     {
+        [Inject]
+        public IConfiguration Config { get; set; }
+
+        [Inject]
+        public IUserHttpService UserService { get; set; }
+
         [Inject]
         public IMovieListHttpService MovieListService { get; set; }
 
@@ -15,8 +24,30 @@ namespace MovieApp.Web.Components.Lists
         [Parameter]
         public string MovieListId { get; set; }
 
+        public UserDto User { get; set; } = new UserDto();
+        public MovieListDto List { get; set; } = new MovieListDto();
+
+        public string PlaceholderImageUrl { get; set; }
+
         protected override async Task OnInitializedAsync()
         {
+            var user = await UserService.GetUserAsync(int.Parse(UserId));
+            var movieList = await MovieListService.GetMovieListAsync(int.Parse(UserId), int.Parse(MovieListId));
+
+            User = user;
+            List = movieList;
+
+            PlaceholderImageUrl = ImageHelper.GetPlaceholderImageUrl(new ImageSettings(Config["TMDB:PlaceholderImageBaseUrl"], 500, 750));
+        }
+
+        protected async Task HandleEditBtnClick()
+        {
+
+        }
+
+        protected async Task HandleDeleteBtnClick()
+        {
+
         }
     }
 }
